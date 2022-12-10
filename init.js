640c0f5d81011d3f8f683ae83af75836e69f8c1d
yunhanyuu <yunhanyu91@gmail.com>
2022-12-10 11:53:19 +0800

    2022/12/10

---
+++ init.js
@@ -0,0 +1,32 @@
+import * as THREE from "https://threejs.org/build/three.module.js"
+import {makeCar,keyframe} from "https://yunhanyuu.github.io/Lab/makeCar.js"
+import * as THREE from 'https://cdn.skypack.dev/three@0.136';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/OrbitControls.js';
+var camera, scene, renderer;
+function init() {
+
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x888888);
+  document.body.appendChild(renderer.domElement);
+
+  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.set(300, 400, 500); // camera at (0,0,500)
+  let controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  ////////////////////////////////////////////////////////
+  
+  scene.add(makeCar());
+  ///////////////////
+}
+function animate() {
+
+  keyframe(clock.getElapsedTime());
+
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+export {init,animate};
+export{camera,scene,renderer};
--- makeCar.js
+++ makeCar.js
@@ -30,3 +30,4 @@ export function keyframe(t) {
   car.position.lerpVectors(keys[ii][1], keys[ii + 1][1], a);
   car.quaternion.slerpQuaternions(keys[ii][2], keys[ii + 1][2], a);
 }
+export { clock };
