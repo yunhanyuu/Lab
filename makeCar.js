7c81f321fe736abf44343947ecf6f09861dee69d
yunhanyuu <113113933+yunhanyuu@users.noreply.github.com>
2022-12-10 12:44:10 +0800

    Update makeCar.js

--- makeCar.js
+++ makeCar.js
@@ -1,10 +1,10 @@
 import * as THREE from "https://threejs.org/build/three.module.js"
 
-export var keys;
+ var keys;
 var T = 5;
-export var clock = new THREE.Clock();
+ var clock = new THREE.Clock();
 var ts = clock.getElapsedTime();
-export function makeCar() {
+ function makeCar() {
   var car = new THREE.Group();
   var normalMat = new THREE.MeshNormalMaterial({
     wireframe: true
@@ -15,7 +15,7 @@ export function makeCar() {
   car.add(body, nose);
   return car;
 }
-export function keyframe(t) {
+ function keyframe(t) {
   var s = ((t - ts) % T) / T;
 
   for (var i = 1; i < keys.length; i++) {
@@ -30,4 +30,6 @@ export function keyframe(t) {
   car.position.lerpVectors(keys[ii][1], keys[ii + 1][1], a);
   car.quaternion.slerpQuaternions(keys[ii][2], keys[ii + 1][2], a);
 }
+export {makeCar, keyframe};
+export {keys, clock};
 
